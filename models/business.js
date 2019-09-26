820f8a45342c4a4d4cdd6937aaea73438515d422
Billy Bright <billybright1992@gmail.com>
2019-09-25 21:32:13 -0700

    data for reviews added

--- models/business.js
+++ models/business.js
@@ -1,36 +1,16 @@
 module.exports = function(sequelize, DataTypes) {
   // Creates a "reviews" model that matches up with DB
-  var Business = sequelize.define("business", {
+  var Business = sequelize.define("Business", {
     businessId: DataTypes.STRING,
     name: DataTypes.STRING,
     address: DataTypes.STRING,
     city: DataTypes.STRING,
     state: DataTypes.STRING,
     postalCode: DataTypes.STRING,
-    latitude: DataTypes.STRING,
-    longitude: DataTypes.STRING,
-    stars: DataTypes.INTEGER,
-    reviewCount: DataTypes.INTEGER,
-    attributes: {
-      RestaurantsTakeOut: DataTypes.STRING,
-      BusinessParking: {
-        garage: DataTypes.BOOLEAN,
-        street: DataTypes.BOOLEAN,
-        validated: DataTypes.BOOLEAN,
-        lot: DataTypes.BOOLEAN,
-        valet: DataTypes.BOOLEAN
-      }
-    },
-    categories: [DataTypes.STRING],
-    hours: {
-      Monday: DataTypes.STRING,
-      Tuesday: DataTypes.STRING,
-      Wednesday: DataTypes.STRING,
-      Thursday: DataTypes.STRING,
-      Friday: DataTypes.STRING,
-      Saturday: DataTypes.STRING,
-      Sunday: DataTypes.STRING
-    }
+    latitude: DataTypes.FLOAT,
+    longitude: DataTypes.FLOAT,
+    stars: DataTypes.FLOAT,
+    reviewCount: DataTypes.INTEGER
   });
 
   return Business;
--- models/reviews.js
+++ models/reviews.js
@@ -1,6 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
   // Creates a "reviews" model that matches up with DB
-  var Reviews = sequelize.define("reviews", {
+  var Reviews = sequelize.define("Reviews", {
     reviewId: DataTypes.STRING,
     userId: DataTypes.STRING,
     businessId: DataTypes.STRING,
