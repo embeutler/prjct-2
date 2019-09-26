d802bfa3452b034a8bab578b419a4e78841c813d
Billy Bright <billybright1992@gmail.com>
2019-09-25 21:40:45 -0700

    commented out html routs

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,11 +1,11 @@
-var db = require("../models");
+//var db = require("../models");
 
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    console.log("hi");
-    db.Reviews.findAll({}).then(function(dbExamples) {
-      console.log(dbExamples);
-    });
-  });
-};
+// module.exports = function(app) {
+//   // Load index page
+//   app.get("/", function(req, res) {
+//     console.log("hi");
+//     db.Reviews.findAll({}).then(function(dbExamples) {
+//       console.log(dbExamples);
+//     });
+//   });
+// };
