6374157d6e697b206784394db4fc285b38279103
Billy Bright <billybright1992@gmail.com>
2019-09-28 13:32:46 -0700

    api routes for both businesses and reviews tables

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,10 +1,35 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Reviews.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get all reviews
+  app.get("/api/reviews", function(req, res) {
+    db.Reviews.findAll({}).then(function(dbReviews) {
+      res.json(dbReviews);
     });
+  });
+
+  // Gets all businesses
+  app.get("/api/businesses", function(req, res) {
+    db.Businesses.findAll({}).then(function(dbBusinesses) {
+      res.json(dbBusinesses);
+    });
+  });
+
+  // Gets an reviews by id
+  app.get("/api/reviews/:id", function(req, res) {
+    db.Reviews.findAll({ where: { id: req.params.id } }).then(function(
+      dbReviews
+    ) {
+      res.json(dbReviews);
+    });
+  });
+
+  // Delete an business by id
+  app.get("/api/businesses/:id", function(req, res) {
+    db.business
+      .findAll({ where: { id: req.params.id } })
+      .then(function(dbBusinesses) {
+        res.json(dbBusinesses);
+      });
   });
 };
