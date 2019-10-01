744d0800ac52b2e342c7d08636502b4764c52eb6
travis rodowick <travrodowick@yahoo.com>
2019-09-30 20:35:55 -0700

    pre merge commit -- changes to association

--- models/business.js
+++ models/business.js
@@ -1,7 +1,13 @@
 module.exports = function(sequelize, DataTypes) {
   // Creates a "Businesses" model that matches up with DB
   var Businesses = sequelize.define("Businesses", {
-    businessId: DataTypes.STRING,
+    businessId: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: false,
+      unique: true
+    },
     name: DataTypes.STRING,
     address: DataTypes.STRING,
     city: DataTypes.STRING,
@@ -12,6 +18,12 @@ module.exports = function(sequelize, DataTypes) {
     stars: DataTypes.FLOAT,
     reviewCount: DataTypes.INTEGER
   });
+
+  Businesses.associate = function(models) {
+    Businesses.hasMany(models.Reviews, {
+      foreignKey: "businessId"
+    });
+  };
 
   return Businesses;
 };
