4dad3b9167c7be267403727448b2dd98e532afac
Billy Bright <billybright1992@gmail.com>
2019-10-02 18:09:15 -0700

    added my linkedin and protfolio page

--- public/assets/js/searchItems.js
+++ public/assets/js/searchItems.js
@@ -4,9 +4,4 @@ $(document).ready(function() {
   $("#search").on("click", function(event) {
     console.log(event);
   });
-
-  function handleSearch(event) {
-    event.preventDefalt();
-    console.log(event);
-  }
 });
