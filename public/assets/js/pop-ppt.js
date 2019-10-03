04cac6e7d7870d44a06914af312afc59925a76dd
Melanie <melaniemok530@gmail.com>
2019-10-02 17:00:40 -0700

    Removed vertical line in footer

--- public/assets/js/pop-ppt.js
+++ public/assets/js/pop-ppt.js
@@ -1,9 +1,8 @@
-$(".popup").click(function() {
-  <iframe
-    src="https://onedrive.live.com/embed?cid=CD3E4BBDB4A228F8&resid=CD3E4BBDB4A228F8%21114&authkey=ACtLWcEUlf4ENQY&em=2"
-    width="1500"
-    height="800"
-    frameborder="0"
-    scrolling="no"
-  ></iframe>;
+$(document).ready(function() {
+  $(".image-link").magnificPopup({ type: "image" });
+});
+
+// A $( document ).ready() block.
+$(document).ready(function() {
+  alert("ready!");
 });
