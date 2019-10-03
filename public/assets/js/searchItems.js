4f7ef6b5957d9db3ea8a1687e955a1e3abe46ddd
Billy Bright <billybright1992@gmail.com>
2019-10-02 17:52:15 -0700

    the search bar is listening now and commented out the power point

--- public/assets/js/searchItems.js
+++ public/assets/js/searchItems.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-  $(document).on("click", ".searchTerm", handleSearch);
+  //$(document).on("click", "#search", handleSearch);
+
+  $("#search").on("click", function(event) {
+    console.log(event);
+  });
 
   function handleSearch(event) {
+    event.preventDefalt();
     console.log(event);
   }
 });
