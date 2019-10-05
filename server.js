1d237549e22d60fc4568b31659da9f7ef84e1860
Melanie <melaniemok530@gmail.com>
2019-10-04 18:34:31 -0700

    Handlebars added to footer and header, page showing properly through the complete use of handlebars

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(express.static("public"));
 app.engine(
   "handlebars",
   exphbs({
-    defaultLayout: "main"
+    defaultLayout: "main",
+    partialsDir: __dirname + "/views/partial/"
   })
 );
 app.set("view engine", "handlebars");
